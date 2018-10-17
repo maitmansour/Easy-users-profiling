a765d97f8e527abcb926f4cb248d254bf78123e7
AIT MANSOUR Mohamed <mohamedaitmansour95@gmail.com>
2018-10-17 16:06:00 +0200

    init project

---
+++ data/produits.js
@@ -0,0 +1,377 @@
+var data=[
+   {
+      "id":"1",
+      "category":"cinema",
+      "title":"Léonardo Dicaprio",
+      "description":"Comédien passionné depuis son plus jeune âge, il fait ses premiers pas devant la caméra à seulement 10 ans. Son interprétation dans Gilbert Grapes est saisissante et révèle son talent précoce. Mais c'est son rôle dans Titanic qui le propulse au rang de star planétaire. Il enchaîne par la suite les grands films et est sacré aux Oscars en 2016. Créer une statue de cire de Leonardo Dicaprio était donc une évidence pour le musée Grévin !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"2",
+      "category":"cinema",
+      "title":"Monica Bellucci",
+      "description":"Mannequin et actrice italienne, Monica Bellucci débute sa carrière chez Elite et Dolce&Gabbana. Elle décrochera ses premiers rôles au cinéma en Italie et se fera connaître à l'international en prenant part à de grosses productions. Sublime Italienne, elle figure régulièrement dans les listes des « plus belles femmes du monde ». La statue de cire de Monica Bellucci vous attend au musée Grévin !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"3",
+      "category":"cinema",
+      "title":"Charlie Chaplin",
+      "description":"Acteur, réalisateur et scénariste de génie, il devient avec son personnage Charlot, une icône du cinéma muet. En 65 ans de carrière, il réalise plus de 80 films, dont certains sont considérés aujourd'hui comme des chefs-d'œuvre. Il reçoit un Oscar d'honneur en 1972. La statue de cire de Charlie Chaplin dit Charlot vous attend au Musée Grévin !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"4",
+      "category":"cinema",
+      "title":"Marilyn Monore",
+      "description":"Marilyn Monroe est repérée en 1944 sur un cliché de propagande de l'armée américaine. Encouragée par ce succès elle se lance dans une carrière de mannequin puis d'actrice. En se créant son personnage de pin-up blonde au grain de beauté mythique, elle devient très vite une icône planétaire. La statue de cire de Marilyn Monroe accueille nos visiteurs au musée Grévin depuis des années !",
+      "weights":{
+         "is_done":false,
+         "male":"0.0",
+         "female":"0.0",
+         "child":"0.0",
+         "teen":"0.0",
+         "adult":"0.0",
+         "old":"0.0"
+      }
+   },
+   {
+      "id":"5",
+      "category":"cinema",
+      "title":"Jean Reno",
+      "description":"Jean Reno,  est aujourd'hui l'un des acteurs français les plus connus au monde. Sa notoriété explose en 1988 avec \"Le Grand Bleu\" de Luc Besson. Il a depuis incarné de nombreux rôles au cinéma et au théâtre. Il a également fait carrière à Hollywood dans \"Mission Impossible\", étendant ainsi sa popularité à l'international.  La statue de cire de Jean Reno accueille nos visiteurs au musée Grévin depuis des années !",
+      "weights":{
+         "is_done":false,
+         "male":"0.0",
+         "female":"0.0",
+         "child":"0.0",
+         "teen":"0.0",
+         "adult":"0.0",
+         "old":"0.0"
+      }
+   },
+   {
+      "id":"6",
+      "category":"france",
+      "title":"Jean La Fontaine",
+      "description":"Ce poète français est principalement connu pour ses fables considérées comme un des chefs d’œuvre de la littérature française. Il met en scène des animaux anthropomorphes et offre au début ou à la fin de sa fable une morale. Il est reçu à l'Académie française en 1684. La statue de cire de Jean de la Fontaine vous attend au musée Grévin.",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"7",
+      "category":"france",
+      "title":"Louis XIV",
+      "description":"De son château de Versailles, Louis XIV, appelé le Roi Soleil, règne durant 72 ans. Ce monarque absolu dirige son royaume d'une main de fer. Sa fin de règne sera moins glorieuse : revers militaires, exode des protestants persécutés, révolte des Camisards et deux famines. La statue de cire du Roi soleil Louis XIV vous attend au musée Grévin.",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"8",
+      "category":"france",
+      "title":"Mme de Pompadour",
+      "description":"La marquise de Pompadour est la favorite de Louis XV. Très vite après leur première rencontre, il l'installe à Versailles dans un appartement proche du sien. Femme cultivée et de goût, mécène des arts, elle conseille le roi. Amie des philosophes, elle favorise la diffusion de l'esprit des lumières. La statue de cire de Mme de Pompadour vous attend au musée Grévin.",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"9",
+      "category":"france",
+      "title":"Napoléon Premier",
+      "description":"Sacré empereur en 1804. Son esprit de conquête conduit les armées françaises du sud à l'est de l'Europe. Ces sanglants revers militaires lui font perdre le soutien des Français. Il abdique en 1814. En 1815, il revient au pouvoir mais la défaite de Waterloo provoque sa chute et son exil. La statue de cire de Napoléon 1er vous attend au musée Grévin.",
+      "weights":{
+         "is_done":false,
+         "male":"0.0",
+         "female":"0.0",
+         "child":"0.0",
+         "teen":"0.0",
+         "adult":"0.0",
+         "old":"0.0"
+      }
+   },
+   {
+      "id":"10",
+      "category":"france",
+      "title":"Albert Einstein",
+      "description":"Physicien américain d’origine allemande, Albert Einstein a révolutionné le monde scientifique. Il inaugure une nouvelle ère de la physique avec ses théories sur la relativité. Sa découverte la plus célèbre, l’équation E=mc2, établit une équivalence entre la matière et l’énergie d’un système. La statue de cire d'Albert Einstein vous attend au musée Grévin.",
+      "weights":{
+         "is_done":false,
+         "male":"0.0",
+         "female":"0.0",
+         "child":"0.0",
+         "teen":"0.0",
+         "adult":"0.0",
+         "old":"0.0"
+      }
+   },
+   {
+      "id":"11",
+      "category":"sport",
+      "title":"Teddy Riner",
+      "description":"Teddy Riner est un judoka qui combat dans la catégorie des poids lourds. Il débute le judo à l'âge de 5 ans. En 2007, à Rio de Janeiro, il devient le plus jeune champion du monde dans sa catégorie. Il enchaîne par la suite les victoires européennes et mondiales, et détient en 2014 le record de dix titres de champion du monde. La statue de cire de Teddy Riner était un ajout incontournable au Musée Grévin !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"12",
+      "category":"sport",
+      "title":"Cristiano Ronaldo",
+      "description":"Cristiano Ronaldo fait ses débuts de footballeur à l’âge de onze ans. C’est au sein du mythique club de Manchester United que Ronaldo se forge un nom, avant de rejoindre le Real Madrid en 2009. Actuel meilleur buteur de la Ligue des Champions, il est un des sportifs les plus médiatisés de sa génération. La statue de cire de Cristiano Ronaldo était un ajout incontournable au Musée Grévin !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"13",
+      "category":"sport",
+      "title":"Camille Lacourt",
+      "description":"Camille Lacourt est un nageur français, spécialiste des épreuves de dos (50 et 100 m). Il s’illustre dès l’adolescence aux championnats de France cadets et juniors. Il est plusieurs fois titré, en 2011, il devient le premier champion du monde français de l'histoire de la natation, grâce à son temps de 52 s 76 centièmes à l'épreuve du 100 m dos. La statue de cire de Camille Lacourt était un ajout incontournable au Musée Grévin !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"14",
+      "category":"sport",
+      "title":"Tony Parker",
+      "description":"Tony Parker est un joueur de basketball international français. Fils d’un basketteur américain, il prend goût au basket et suit un cursus sport-études. A 21 ans, il devient le premier Français champion de NBA et il est, en 2007, le premier Européen à recevoir le titre de meilleur joueur des finales de NBA.  La statue de cire de Tony Parker était un ajout incontournable au Musée Grévin !",
+      "weights":{
+         "is_done":false,
+         "male":"0.0",
+         "female":"0.0",
+         "child":"0.0",
+         "teen":"0.0",
+         "adult":"0.0",
+         "old":"0.0"
+      }
+   },
+   {
+      "id":"15",
+      "category":"sport",
+      "title":"Renaud Lavillenie",
+      "description":"Renaud Lavillenie est un athlète français, détenteur du record du monde de saut à la perche depuis 2014 avec un saut à 6,16 mètres. Il enchaîne les titres nationaux et internationaux, de Champion de France à Champion Olympique. La statue de cire de Renaud Lavillenie était un ajout incontournable au Musée Grévin !",
+      "weights":{
+         "is_done":false,
+         "male":"0.0",
+         "female":"0.0",
+         "child":"0.0",
+         "teen":"0.0",
+         "adult":"0.0",
+         "old":"0.0"
+      }
+   },
+   {
+      "id":"16",
+      "category":"art",
+      "title":"Maitre Gims",
+      "description":"Maître Gims, auteur, compositeur et interprète, est à la tête de nombreux tubes et de plus de 5 millions d’albums vendus. Son titre « Sapé comme jamais » fait danser la France entière, il bat des records sur Youtube avec plus de 300 millions de vues en Août 2017 et lui vaut une victoire de la musique en 2016.  La statue de cire de Maître Gims vous attend au Musée Grévin !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"17",
+      "category":"art",
+      "title":"Kendji Girac",
+      "description":"Kendji Girac connaît le succès en remportant à 17 ans la saison 3 de l’émission The Voice - La Plus Belle Voix. Après sa victoire, il fait danser la France sur son tube Color Gitano et ne s’arrête pas là. Ses origines catalanes et gitanes sont sa première source d’inspiration pour chanter, aussi bien en français qu’en espagnol. La statue de cire de Kendji Girac vous attend au Musée Grévin !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"18",
+      "category":"art",
+      "title":"Katy Perry",
+      "description":"Katy Perry, chanteuse pop aux indénombrables récompenses, des Teen Choice Awards aux Golden Globes en passant par les MTV Europe Music Awards, est une figure emblématique de l’univers musical actuel. Auteur-Compositrice-Interprète, elle se fait connaître à l’international avec le tube I kissed a girl, elle se démarquera avec ses tenues toutes plus colorées et audacieuses les unes que les autres. La statue de cire de Katy Perry vous attend au Musée Grévin !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"19",
+      "category":"art",
+      "title":"Stromae",
+      "description":"Stromae est un auteur-compositeur-interprète et producteur belge. Très jeune, il s’intéresse à la musique. Compositeur talentueux et innovateur, il se fait aussi connaître grâce à des vidéos sur internet : Les leçons de Stromae. En 2009, son single Alors on danse fait danser toute l’Europe. Il enchaîne depuis les succès à chacun de ses albums. La statue de cire de Stromae vous attend au Musée Grévin !",
+      "weights":{
+         "is_done":false,
+         "male":"0.0",
+         "female":"0.0",
+         "child":"0.0",
+         "teen":"0.0",
+         "adult":"0.0",
+         "old":"0.0"
+      }
+   },
+   {
+      "id":"20",
+      "category":"art",
+      "title":"Edith Piaf",
+      "description":"Edith Piaf est l’une des figures emblématiques de la chanson populaire française. Elle développe un don unique pour le chant pendant son enfance.  Elle est repérée à Paris par Louis Leplée le gérant du Gerny's, qui l’engage et lui donne son nom de scène: « La Môme Piaf ». C’est le début d’une carrière exceptionnelle. La statue de cire d'Edith Piaf est l'une des stars du Musée Grévin !",
+      "weights":{
+         "is_done":false,
+         "male":"0.0",
+         "female":"0.0",
+         "child":"0.0",
+         "teen":"0.0",
+         "adult":"0.0",
+         "old":"0.0"
+      }
+   },
+   {
+      "id":"21",
+      "category":"comics",
+      "title":"Le petit Prince",
+      "description":"Le Petit Prince est un jeune garçon aux cheveux d’or qui parcourt l’univers en quête d’amitié jusqu’à son arrivée sur Terre. Créé par Antoine de Saint-Exupéry. Phénomène éditorial, c’est l’ouvrage de littérature française le plus traduit, le plus lu et le plus connu dans le monde. La statue de cire du Petit Prince trouve sans peine sa place dans l'univers onirique et poétique du Musée Grévin ! ",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"22",
+      "category":"comics",
+      "title":"Scrat",
+      "description":"Dans le film d'animation L'Âge de Glace, Scrat est un petit écureuil préhistorique obnubilé par l'unique gland ayant survécu à la fonte des glaces. Personnage attachant et malchanceux, il devient une figure emblématique de la saga. La statue de Scrat plaît beaucoup aux enfants... petits et grands !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"23",
+      "category":"comics",
+      "title":"Aladin",
+      "description":"Aladdin est un personnage de fiction qui est apparu pour la première fois en 1992 dans le long métrage d'animation Aladdin. Il est inspiré du personnage homonyme, héros du conte des Mille et Une Nuits, Aladin et la Lampe merveilleuse !",
+      "weights":{
+         "is_done":true,
+         "male":"0.5",
+         "female":"0.8",
+         "child":"0.1",
+         "teen":"0.3",
+         "adult":"0.3",
+         "old":"0.1"
+      }
+   },
+   {
+      "id":"24",
+      "category":"comics",
+      "title":"Bart Simpson",
+      "description":"Bartholomew JoJo « Bart » Simpson est un des personnages principaux de la série télévisée d'animation Les Simpson et fait partie de la famille éponyme !",
+      "weights":{
+         "is_done":false,
+         "male":"0.0",
+         "female":"0.0",
+         "child":"0.0",
+         "teen":"0.0",
+         "adult":"0.0",
+         "old":"0.0"
+      }
+   },
+   {
+      "id":"25",
+      "category":"comics",
+      "title":"Lara Croft",
+      "description":"Lara Croft est une jeune aventurière, archéologue et chasseuse de trésor, fille d’un riche aristocrate anglais. Créée en 1996 par Toby Gard pour la série de jeux vidéo Tomb Raider, elle est la première héroïne humaine de jeu vidéo. Son succès planétaire a fait d’elle une icône culturelle. Son succès planétaire a fait d’elle une icône culturelle : il était hors de question pour le musée Grévin de ne pas compter sa statue de cire parmi nos invités permanents ! ",
+      "weights":{
+         "is_done":false,
+         "male":"0.0",
+         "female":"0.0",
+         "child":"0.0",
+         "teen":"0.0",
+         "adult":"0.0",
+         "old":"0.0"
+      }
+   }
+];
