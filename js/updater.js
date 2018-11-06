2d6b70a6cc8a26c6f14c06bd20f147e33bb34212
AIT MANSOUR Mohamed <mohamedaitmansour95@gmail.com>
2018-11-06 16:55:05 +0100

    save last weight by product to calculate new weights

--- js/updater.js
+++ js/updater.js
@@ -17,9 +17,23 @@ function updateWeights(user_id, product_id, is_done) {
     for (var i = 0; i < clicked_items.length; i++) {
       choosen_products.push(data[clicked_items[i] - 1].weights);
     }
-    new_product_clicks[product_id-1].push(choosen_products);
     var points = [];
+    var new_product_done=true;
     for (var i = 0; i < choosen_products.length; i++) {
+      if (new_product_done) {
+                  if (new_product_clicks[product_id-1].length>0) {
+                 points.push([
+        new_product_clicks[product_id-1][0], new_product_clicks[product_id-1][0],
+        new_product_clicks[product_id-1][1], new_product_clicks[product_id-1][1],
+        new_product_clicks[product_id-1][2], new_product_clicks[product_id-1][2],
+        new_product_clicks[product_id-1][3], new_product_clicks[product_id-1][3],
+        new_product_clicks[product_id-1][4], new_product_clicks[product_id-1][4],
+        new_product_clicks[product_id-1][5], new_product_clicks[product_id-1][5]
+      ]);
+        }
+        new_product_done=false;
+      }
+
       points.push([choosen_products[i].male, choosen_products[i].male,
         choosen_products[i].female, choosen_products[i].female,
         choosen_products[i].child, choosen_products[i].child,
@@ -28,11 +42,12 @@ function updateWeights(user_id, product_id, is_done) {
         choosen_products[i].old, choosen_products[i].old
       ]);
     }
-    console.log("NEW PRODUCT CLICKS : "+JSON.stringify(new_product_clicks));
+    console.log("OK POINTS : "+JSON.stringify(points))
     // get new weight, based on barycenter 
     var new_weight = getCentroid(points);
-    updateCurrentProduct(new_weight);
-    console.log(getCentroid(points));
+    updateCurrentProduct(new_weight,product_id);
+      new_product_clicks[product_id-1]=new_weight;
+        console.log(getCentroid(points));
     clicks = 0;
     clicked_items = [];
     choosen_products = [];
@@ -43,13 +58,11 @@ function updateWeights(user_id, product_id, is_done) {
 
 
 // Suggest a product based on weights
-function updateCurrentProduct(weights) {
+function updateCurrentProduct(weights,product_id) {
   // TOODO compare weight with existing weights and siggest one product
-  var suggested_product = calculateMansourWeight(weights, data);
-  console.log(JSON.stringify(suggested_product));
-  var finded_product = '<h2>Suggested Product : </h2>' +
-    '             <div>         <div class="grid">            <div class="portfolio app mix_all">             <div class="portfolio-wrapper">                   <a data-toggle="modal" data-target="#modal_' + suggested_product.id + '" href="#" class="b-link-stripe b-animate-go  thickbox" style="border-style: solid;">                   <img src="images/produits/' + suggested_product.id + '.png" style="height:186"><div class="b-wrapper"><h2 class="b-animate b-from-left    b-delay03 "><img src="images/link-ico.png" alt=""></h2></div>                  </a>                      </div>              </div>                <p class="text-center">' + suggested_product.title + '</p>                    </div>        </div>';
-  $('.weights_area').append(' <h2>Calculated weights </h2><ul>' +
+  var finded_product = '<h2>Updated Product : </h2>' +
+    '             <div>         <div class="grid">            <div class="portfolio app mix_all">             <div class="portfolio-wrapper">                   <a data-toggle="modal" data-target="#modal_' + product_id + '" href="#" class="b-link-stripe b-animate-go  thickbox" style="border-style: solid;">                   <img src="images/produits/' + product_id + '.png" style="height:186"><div class="b-wrapper"><h2 class="b-animate b-from-left    b-delay03 "><img src="images/link-ico.png" alt=""></h2></div>                  </a>                      </div>              </div>   </div>        </div>';
+  $('.weights_area').append(' <h2>New weights </h2><ul>' +
     '  <li>Male : ' + weights[0] + '</li>' +
     '  <li>Female : ' + weights[1] + '</li>' +
     '  <li>Child : ' + weights[2] + '</li>' +
